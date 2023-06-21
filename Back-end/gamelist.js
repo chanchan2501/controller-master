165cd93482455c51dbd4127c125e0df407e724d4
chanchan2501 <doanthithuhuongbk@gmail.com>
2023-06-21 12:47:51 +0700

    first commit

---
+++ Back-end/gamelist.js
@@ -0,0 +1,20 @@
+db.games.insert([
+  {
+    title: "Hades",
+    genre: "Roguelike",
+    developper: "Supergiant games",
+    read: true,
+  },
+  {
+    title: "Katanazero",
+    genre: "Hack and slash",
+    developper: "Askiisoft",
+    read: true,
+  },
+  {
+    title: "Microsoft mineswepper",
+    genre: "Arcade",
+    developper: "Microsoft Inc",
+    read: true,
+  },
+]);
